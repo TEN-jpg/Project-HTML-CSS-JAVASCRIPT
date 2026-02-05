6b786f5a8c6c2913660f3d91048fe2f13cee3e00
TEN <pratikbansod82@gmail.com>
2026-02-05 18:06:30 +0530

    Added Trading View Clone

---
+++ tradingViewClone/app.js
@@ -0,0 +1,15 @@
+const body = document.body;
+const mode = document.querySelector("#mode");
+let anchor = document.querySelectorAll("a");
+
+mode.addEventListener("click", () => {
+    body.classList.toggle("dark");
+});
+
+
+
+
+
+
+
+
